159b20410d2fe0c7bd7816bbf600e7d29dedb18f
Tobias703 <seiffertt.tin21@student.dhbw-heidenheim.de>
2024-06-21 20:03:33 +0200

    TypeScript Final (?)

--- UI5-TypeScript/webapp/controller/App.controller.ts
+++ UI5-TypeScript/webapp/controller/App.controller.ts
@@ -1,6 +1,11 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import Log from "sap/base/Log";
 
 /**
  * @namespace ui5.typescript.controller
  */
-export default class App extends Controller {}
+export default class App extends Controller {
+    public onInit(): void {
+        Log.setLevel(Log.Level.WARNING);
+    }
+}
--- UI5-TypeScript/webapp/controller/HelloPanel.controller.ts
+++ UI5-TypeScript/webapp/controller/HelloPanel.controller.ts
@@ -1,30 +1,31 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
 import Fragment from "sap/ui/core/Fragment";
+import Dialog from "sap/m/Dialog";
 
 export default class HelloPanel extends Controller {
-    private oDialog : any;
+    private oDialog: any;
 
     public onShowHello(): void {
-        const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        const sMsg = `Hello, ${sRecipient}`;
-
+        const view: any = this.getView();
+        const model: any = view.getModel();
+        const sRecipient: string = model.getProperty("/recipient/name");
+        const sMsg: string = `Hello, ${sRecipient}`;
         MessageToast.show(sMsg);
     }
 
     public async onOpenDialog(): Promise<void> {
-        this.oDialog = this.oDialog ?? await Fragment.load({
-            name: "ui5.javascript.fragment.HelloDialog",
+        this.oDialog ??= await Fragment.load({
+            name: "ui5.typescript.view.HelloDialog",
             controller: this
-        });
-
+        }) as Dialog;
+        const view: any = this.getView();
+        const oModel = view.getModel();
+        this.oDialog.setModel(oModel);
         this.oDialog.open();
     }
 
     public onCloseDialog(): void {
-        const dialog : any = this.byId("helloDialog");
-        if (dialog) {
-            dialog.close();
-        }
+        this.oDialog.close();
     }
 }
