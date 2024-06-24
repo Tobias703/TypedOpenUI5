48650ed1de0128aa56baea6c7e02536f38baaff3
Tobias703 <seiffertt.tin21@student.dhbw-heidenheim.de>
2024-06-24 14:24:55 +0200

    BasicTypesJSDoc

--- JSDoc/jsdoc/BasicTypes.js
+++ JSDoc/jsdoc/BasicTypes.js
@@ -9,16 +9,36 @@ function foobar(foo, bar) {
     return foo + bar;
 }
 
+/**
+ * Addiert zwei Zahlen
+ * @param {number} foo
+ * @param {number} bar
+ * @returns {number}
+ */
+function barfoo(foo, bar) {
+    return foo + bar;
+}
+
 foobar("foo", 5);
 foobar("foo", (5).toString());
 
-/**
- * @type {string} foo
- */
+/** @type {string} foo */
 var foo = 42;
 var foo = "bar";
 
 /** @type {string} bar */
 var bar = "foo";
-/** @type {string} bar */ var bar = "bar";
-/** @type {number} bar */ var bar = 42;
+/** @type {string} bar */
+var bar = "bar";
+/** @type {number} bar */
+var bar = 42;
+
+/** 
+ * @type {string} foo 
+ */
+var foo = "foo";
+
+/** @type {string} foo */
+var foo = "foo";
+
+/** @type {string} foo */ var foo = "foo";
